1adc523a80f7c847f1d57b2fa10bb40d7e09263b
Tumble <tumblegamer@tumblenet.tk>
2016-06-20 20:38:30 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,7 +1,7 @@
 $(function(){
  function printOutput() {
   var obj = JSON.parse(this.responseText);
-  var face = jQuery.getJSON("https://api.github.com/repos/tumblegamer/TumbleCraft/releases?callback=?",callback)
+  var face = jQuery.getJSON("https://api.github.com/repos/tumblegamer/TumbleCraft/releases?callback=0",0)
   
   var text="";
   /*for (i = 0; i < obj.data.length; i++) { 
