d6002b106291b5f370129c517a7a7628f1112d6b
Tumble <tumblegamer@tumblenet.tk>
2016-06-20 19:45:56 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,10 +1,23 @@
 $(function(){
  function printOutput() {
-  var responseObj = JSON.parse(this.responseText);
-  $("#releases").html(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
+  var obj = JSON.parse(this.responseText);
+  $("#releases").html(obj[1].id);
 }
-var request = new XMLHttpRequest();
+function githubAIP(DOMString method,
+   DOMString url,
+   optional boolean async,
+   optional DOMString user,
+   optional DOMString password)
+   {
+    var request = new XMLHttpRequest();
 request.onload = printOutput;
-request.open('get', 'https://api.github.com/users/tumblegamer', true)
+request.open(method,"https://api.github.com" + url,async,user,password)
 request.send()
+   }
+githubAPI(get,"/repos/tumblegamer/TumbleCraft/releases")
 });
+
+
+
+
+
