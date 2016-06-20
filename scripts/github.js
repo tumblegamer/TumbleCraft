fd3098ddecb9599f82782c9d32f63a4e54c74671
Tumble <tumblegamer@tumblenet.tk>
2016-06-20 17:36:07 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,8 +1,10 @@
-function printRepoCount() {
-  var responseObj = JSON.parse(this.responseText);
-  console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
+$(document).ready(function(){
+  function printRepoCount() {
+    var responseObj = JSON.parse(this.responseText);
+    console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
+  }
+  var request = new XMLHttpRequest();
+  request.onload = printRepoCount;
+  request.open('get', 'https://api.github.com/users/funchal', true)
+  request.send()
 }
-var request = new XMLHttpRequest();
-request.onload = printRepoCount;
-request.open('get', 'https://api.github.com/users/funchal', true)
-request.send()
