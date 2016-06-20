115c3c8cde1594f11d542334641806c01edc75dc
Tumble <tumblegamer@tumblenet.tk>
2016-06-20 17:43:55 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  
+  
+  
   function printRepoCount() {
     var responseObj = JSON.parse(this.responseText);
     console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
@@ -6,5 +9,6 @@ $(document).ready(function(){
   var request = new XMLHttpRequest();
   request.onload = printRepoCount;
   request.open('get', 'https://api.github.com/users/funchal', true)
+  $("#releases").html("<b>Hello world!</b>");
   request.send()
 });
