3f4259a770431ed0d8f8c0ae4f592f9001dc355a
Tumble <tumblegamer@tumblenet.tk>
2016-06-20 17:30:39 +0100

    Create github.js

---
+++ scripts/github.js
@@ -0,0 +1,8 @@
+function printRepoCount() {
+  var responseObj = JSON.parse(this.responseText);
+  console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
+}
+var request = new XMLHttpRequest();
+request.onload = printRepoCount;
+request.open('get', 'https://api.github.com/users/funchal', true)
+request.send()
