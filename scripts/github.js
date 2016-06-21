cde891676a1eb7227fe75dc508a42f7bf57c4507
Tumble <tumblegamer@tumblenet.tk>
2016-06-21 19:04:49 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,11 +1,10 @@
 function githubAPI(url) {
-  url="https://api.github.com" + url
-   $(htmlTag).html("Loading GitHub API...");
-   var output = [];
+  url="https://api.github.com" + url;
+  var output = [];
    
-    $.getJSON(url, function(data) {
-      output = data;
-    }
+  $.getJSON(url, function(data) {
+    output = data;
+  }
     
-    return output;
+  return output;
 });
