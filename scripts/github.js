a2b28df38d164cbcd8943f9259e3c6445b94bad7
Tumble <tumblegamer@tumblenet.tk>
2016-06-21 18:08:57 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -3,13 +3,13 @@ $(function() {
   githubApiUrl = "https://api.github.com";
 
   function githubAPI(url) {
-    $("#releases").html("Loading GitHub API..,");
+    $("#releases").html("Loading GitHub API...");
 
     $("#releases").getJSON(url, function(data) {
       if (data.length == 0) {
         text = text + '<p>No releases!</p>';
       } else {
-        $.each(data, function(element) {
+        $.each(data, function(index, element) {
           text += "<li>" + element.name + "</li>";
         });
         text = "<ul>" + text + "</ul>";
