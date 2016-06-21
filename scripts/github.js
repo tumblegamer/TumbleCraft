6563e56a7cf46edfc1853051201e3e08213eb02d
Tumble <tumblegamer@tumblenet.tk>
2016-06-21 17:59:15 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,28 +1,24 @@
-$(function(){
- 
- githubApiUrl = "https://api.github.com";
- function githubAPI(url){
-  $(this).html("Loading GitHub API..,");
-  
-  $(this).getJSON(url, function(data){
-   if(data.length==0) {
-    text = text + '<p>No releases!</p>'; 
-   } else {
-    $.each(data, function(element) {
-     text += "<li>" + element.name + "</li>";
+$(function() {
+
+  githubApiUrl = "https://api.github.com";
+
+  function githubAPI(url) {
+    $(this).html("Loading GitHub API..,");
+
+    $(this).getJSON(url, function(data) {
+      if (data.length == 0) {
+        text = text + '<p>No releases!</p>';
+      } else {
+        $.each(data, function(element) {
+          text += "<li>" + element.name + "</li>";
+        });
+        text = "<ul>" + text + "</ul>";
+      }
+      $(this).html(text);
     });
-    text = "<ul>" + text + "</ul>";
-   }
-    $(this).html(text);
-  });
- }
- var username = "tumblegamer";
- var jsonUrl = githubApiUrl + "/repos/tumblegamer/TumbleCraft/releases";
- $("#releases").html("Loading JSON...");
- $("#releases").githubAPI(jsonUrl);
+  }
+  var username = "tumblegamer";
+  var jsonUrl = githubApiUrl + "/repos/tumblegamer/TumbleCraft/releases";
+  $("#releases").html("Loading JSON...");
+  $("#releases").githubAPI(jsonUrl);
 });
-
-
-
-
-
