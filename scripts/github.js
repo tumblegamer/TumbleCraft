c3fd4e256256fc5b37d6cfd00fe1d80931f87366
Tumble <tumblegamer@tumblenet.tk>
2016-06-21 15:56:40 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -1,13 +1,12 @@
 $(function(){
  function printOutput() {
   var obj = JSON.parse(this.responseText);
-  var face = jQuery.getJSON("https://api.github.com/repos/tumblegamer/TumbleCraft/releases?callback=0",0);
   
   var text="";
   /*for (i = 0; i < obj.data.length; i++) { 
    text += obj.data[i].id + "<br>";
   }*/
-  $("#releases").html(face.data[1].id);
+  $("#releases").html(obj[1].id);
 }
 
   
@@ -20,13 +19,10 @@ function githubAIP(DOMString method,
    {
     var request = new XMLHttpRequest();
 request.onload = printOutput;
-request.open(method,"https://api.github.com" + url,async,user,password);
-request.send();
+request.open(method,"https://api.github.com" + url,async,user,password)
+request.send()
    }
-githubAPI(get,"/repos/tumblegamer/TumbleCraft/releases?callback=0");
-var face = jQuery.getJSON("https://api.github.com/repos/tumblegamer/TumbleCraft/releases?callback=0",0);
-  $("#releases").html(face.data[1].id);
-  $("#releases").html("test");
+githubAPI(get,"/repos/tumblegamer/TumbleCraft/releases")
 });
 
 
