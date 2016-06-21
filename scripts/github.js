a88dacb906beb7676fc5d1869c453957ea54f5f8
Tumble <tumblegamer@tumblenet.tk>
2016-06-21 17:46:12 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -2,24 +2,24 @@ $(function(){
  
  githubApiUrl = "https://api.github.com";
  function githubAPI(url){
-  $("#releases").html("Loading GitHub API..,");
+  $(this).html("Loading GitHub API..,");
   
-  $.getJSON(url, function(data){
+  $(this).getJSON(url, function(data){
    if(data.length == 0) {
     text = text + '<p>No releases!</p>'; 
    } else {
     $.each(data, function(element) {
      text += "<li>" + element.name + "</li>";
     }
-    text = "<ul>" + text + "</ul>"
+    text = "<ul>" + text + "</ul>";
    }
-    $("#releases").html(text);
+    $(this)..html(text);
   });
  }
  var username = "tumblegamer";
  var jsonUrl = githubApiUrl + "/repos/tumblegamer/TumbleCraft/releases";
  $("#releases").html("Loading JSON...");
- githubAPI(jsonUrl);
+ $("#releases").githubAPI(jsonUrl);
 });
 
 
