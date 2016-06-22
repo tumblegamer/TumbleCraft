022475193e48c7cae661716ccf26d841b611d494
Tumble <tumblegamer@tumblenet.tk>
2016-06-22 11:37:18 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -2,7 +2,7 @@ $(function(){
       var text = "";
       var releases = githubAPI("/repos/tumblegamer/TumbleCraft/releases");
       alert(releases.toString());
-      $.each(releases, function(index,element) {
+      $.each(githubAPI("/repos/tumblegamer/TumbleCraft/releases"), function(index,element) {
           text = text + "<li>" + element.name + "<br><a href=" + element.zipball_url + ">Download ZIP</a> | <a href=" + element.tarball_url + ">Download TAR</a> | <a href=" + element.html_url + ">Change Log</a></li><br>";
       });
       text = "<ul>" + text + "</ul>";
