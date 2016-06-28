77758594c7f3963a7f57ebcde016244b2fd41b05
Tumble <tumblegamer@tumblenet.tk>
2016-06-28 14:49:27 +0100

    Update github.js

--- scripts/github.js
+++ scripts/github.js
@@ -6,7 +6,7 @@ $(function(){
           text = text + "<li>" + element.name + "<br><a href=" + element.zipball_url + ">Download ZIP</a> | <a href=" + element.tarball_url + ">Download TAR</a> | <a href=" + element.html_url + ">Change Log</a></li><br>";
       });
       text = "<ul>" + text + "</ul>";
-      alert(text);
+      //alert(text);
       alert(releasesArray.toString());
       
       $("#releases").html(text);
